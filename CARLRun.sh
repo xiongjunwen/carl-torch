38bbca454fff116647dc00c221e9daf3a664d383
xiongjunwen <junwenxiong@lbl.gov>
2022-11-14 13:51:53 +0100

    correct error for batch submit

--- CARLRun.sh
+++ CARLRun.sh
@@ -3,11 +3,11 @@
 # extract the files 
 tar xvf carl-torch-lubos.tar.gz
 # activate the env 
-source CARLENV/bin/activate
+source CarlEnv/bin/activate
 
 # run the training 
 python train.py -n Wjets_Sh221 -v Wjets_MGPy8 -e -1 \
--p /eos/user/${USER:0:1}/${USER}/workdir/hcc/carl/ -g sh221-wjets-vhbb-lubos-batch \
+-p /eos/user/${USER:0:1}/${USER}/workdir/hcc/carl/carl-inputs/ -g sh221-wjets-vhbb-lubos-batch \
 -t Nominal -f "MET,dPhiLBmin,dPhiVBB,dRBB,dYWH,mBB,mTW,Mtop,pTB1,pTB2,pTV,nTags,nJ,FlavourLabel" \
 -w EventWeight --scale-method minmax --BoolFilter "nTags==2 & nJ <= 3 & FlavourLabel==2"
 
