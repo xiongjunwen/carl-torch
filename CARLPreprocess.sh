55aa60e075ca70013787073e9af97ca3aae25fbd
xiongjunwen <junwenxiong@lbl.gov>
2022-11-24 14:12:57 +0100

    new preprocessing and plotting script correction

--- CARLPreprocess.sh
+++ CARLPreprocess.sh
@@ -7,13 +7,22 @@ cd /afs/cern.ch/user/j/jxiong/workdir/vhcc/carl/carl-torch # change this to your
 echo "preprocessing sh2211 Vhcc z+jets zhf"
 # activate the env 
 source CarlEnv/bin/activate
+
 # preprocess 
+#################################### 11/23/2022 merge 2jet, 3pjet 
+# python preprocess.py \
+# "/eos/user/j/jxiong/workdir/hcc/carl/inputs/vhcc_hadded_all_tree_2L_zjets_sh2211_mva.root" \
+# "EventWeight,mBB,dRBB,dEtaVBB,dPhiVBB,pTV,pTB1,pTB2,METSig,mLL,cosThetaLep,bin_bTagB1,bin_bTagB2,nTags,nJ,FlavourLabel,Description,sample" \
+# -r "Description" "sample" \
+# -n -1 -s "(nTags==2) & (pTV>=75.0) & ((Description==\"SR\") | (Description==\"CRHigh\")) & ((sample==\"Zbb\") | (sample==\"Zcc\"))" \
+# -o /eos/user/j/jxiong/workdir/hcc/carl/preprocessed/vhcc_hadded_all_tree_2L_zjets_sh2211_mva_srcr_ptv75_preprocessed.root
+#################################### 11/24/2022 split 2jet, merge flavors 
 python preprocess.py \
 "/eos/user/j/jxiong/workdir/hcc/carl/inputs/vhcc_hadded_all_tree_2L_zjets_sh2211_mva.root" \
-"EventWeight,mBB,dRBB,dEtaVBB,dPhiVBB,pTV,pTB1,pTB2,METSig,mLL,cosThetaLep,bin_bTagB1,bin_bTagB2,nTags,nJ,FlavourLabel,Description,sample" \
--r "Description" "sample" \
--n -1 -s "(nTags==2) & (pTV>=75.0) & ((Description==\"SR\") | (Description==\"CRHigh\")) & ((sample==\"Zbb\") | (sample==\"Zcc\"))" \
--o /eos/user/j/jxiong/workdir/hcc/carl/preprocessed/vhcc_hadded_all_tree_2L_zjets_sh2211_mva_srcr_ptv75_preprocessed.root
+"EventWeight,mBB,dRBB,dEtaVBB,dPhiVBB,pTV,pTB1,pTB2,METSig,mLL,cosThetaLep,bin_bTagB1,bin_bTagB2,nTags,nJ,FlavourLabel,Description" \
+-r "Description" \
+-n -1 -s "(nTags==2) & (nJ==2) & (pTV>=75.0) & ((Description==\"SR\") | (Description==\"CRHigh\"))" \
+-o /eos/user/j/jxiong/workdir/hcc/carl/preprocessed/vhcc_hadded_all_tree_2L_zjets_sh2211_mva_2jet_allflavors_preprocessed.root
 
 # deactivat env
 deactivate
